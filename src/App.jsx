58c343731c6d34998d20eb7bbfa23a8042609707
RachelGcp <rysy3692@gmail.com>
2025-04-30 14:27:07 +0300

    change coloors and move index

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   const handleClick = () => {
     setText('You clicked me! 🎉');
-    setColor(color === 'black' ? 'red' : 'green');
+    setColor(color === 'black' ? 'pink' : 'blue');
   };
 
   return (
