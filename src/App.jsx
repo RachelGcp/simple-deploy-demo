d7d05ee9eddfc02b0e1c3893499e6bef987f90d6
RachelGcp <rysy3692@gmail.com>
2025-04-30 11:11:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+function App() {
+  const [text, setText] = useState('Hello world!');
+  const [color, setColor] = useState('black');
+
+  const handleClick = () => {
+    setText('You clicked me! 🎉');
+    setColor(color === 'black' ? 'blue' : 'black');
+  };
+
+  return (
+    <div style={{ padding: '3rem', textAlign: 'center' }}>
+      <h1 style={{ color }}>{text}</h1>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
