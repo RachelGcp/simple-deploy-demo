651c356e5d76829546f9f580dc6dc6fb11eb0bff
RachelGcp <rysy3692@gmail.com>
2025-04-30 11:39:56 +0300

    change colors

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   const handleClick = () => {
     setText('You clicked me! 🎉');
-    setColor(color === 'black' ? 'blue' : 'black');
+    setColor(color === 'black' ? 'pink' : 'green');
   };
 
   return (
