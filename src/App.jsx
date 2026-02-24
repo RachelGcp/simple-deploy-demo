5eaabe672c3ef0deff181cffb5dcb1bff0a4bbe4
RachelGcp <rysy3692@gmail.com>
2026-02-24 16:43:06 +0200

    change title to Purim

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 function App() {
-  const [text, setText] = useState('Hello world With Change!');
+  const [text, setText] = useState('Happy Purim!');
   const [color, setColor] = useState('black');
 
   const handleClick = () => {
